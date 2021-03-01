b74e897ce59db1840c92fa0ad1a675c9697c1670
kimyeonah <yeny28@daum.net>
2021-03-02 00:52:41 +0900

    txt file을 읽어오는 기능 - file 내용을 text 변수에 넣어주고 return

--- YA_TestDetector.py
+++ YA_TestDetector.py
@@ -1,1 +1,7 @@
-print("Hello")
+def fileopen(data):
+    with open(data, 'r', encoding='UTF8') as file:
+        text = file.read()
+
+    return text
+
+target_text = fileopen(".\data_file\예시자료2.txt")
