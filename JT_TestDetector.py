0d53b61dde260bb6f660f2e0f89149a9944a3593
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-03-02 01:25:34 +0900

    Add 엑셀의 특정열 불러오는 함수 - 검색할 단어들을 불러올 수 있다.

--- JT_TestDetector.py
+++ JT_TestDetector.py
@@ -1,1 +1,9 @@
-print('hello python')
+import pandas as pd
+
+
+def load_searchWord():
+
+    df = pd.read_excel("./data_file/SearchWords.xlsx", sheet_name=0)  # can also name of sheet
+    my_list = df['단어/용어'].tolist()
+
+    return my_list
